9ec11e96ec20a201dca289ee5e043aa42d879250
Javeria-taj <tajjaveria2006@gmail.com>
2026-01-17 16:40:59 +0530

    Use environment variables for Firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 /* 🔐 Firebase Config */
 const firebaseConfig = {
-  apiKey: "AIzaSyClVU-fmMyHrvXTbOuR--LjivOpVu_UXns",
+  apiKey: "AIzaSyDQE9ZF5TmWEmAs9RVHxF1Vpjjt3UxsbVk",
   authDomain: "ecoguide-90bf6.firebaseapp.com",
   projectId: "ecoguide-90bf6",
   storageBucket: "ecoguide-90bf6.appspot.com",
